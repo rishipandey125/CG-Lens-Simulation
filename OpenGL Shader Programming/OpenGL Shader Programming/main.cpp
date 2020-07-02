0f2992eccaa84e791cacb903acf5ffb7272532c0
rishipandey125 <rpandey2@illinois.edu>
2020-07-02 15:27:55 -0500

    added an xcode project to this repo

---
+++ OpenGL Shader Programming/OpenGL Shader Programming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Shader Programming
+//
+//  Created by Rishi Pandey on 7/2/20.
+//  Copyright © 2020 Rishi Pandey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
